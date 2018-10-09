069732c1679458d23051061297f4136d692926fa
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-08 21:38:51 -0500

    Style chat area with purecss css library

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
   .chat
-    .messages
-      li.message(v-for="message in messages") {{message.author}}: {{message.content}}
-    textarea.new-message(v-model="newMessage")
-    button.send-button(@click="sendMessage(newMessage)") Send
+    .messages.pure-g
+      .message.pure-u-1.pure-g(v-for="message in messages")
+        .author {{message.author}}
+        .content {{message.content}}
+    .new-message.pure-g
+      form.pure-form
+        textarea(type="text" placeholder="Type your message here" v-model="newMessage" @keyup.enter.exact="sendMessage()")
 </template>
 
 <script>
@@ -20,13 +23,14 @@ export default {
     }
   },
   methods: {
-    sendMessage (content) {
+    sendMessage () {
       const payload = {
         author: 'Martin Fracker',
-        content
+        content: this.newMessage
       }
       this.addMessage(payload)
       this.$socket.emit('chatmessage', payload)
+      this.newMessage = ''
     },
     addMessage ({ author, content }) {
       this.messages.push({ author, content })
@@ -34,4 +38,27 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="less">
+.chat {
+  margin: 1rem;
+}
+
+.author {
+  font-weight: 900;
+}
+
+.new-message {
+  form {
+    width: 100%;
+  }
+  textarea {
+    width: 100%;
+  }
+}
+
+.messages + .new-message {
+  margin-top: 1rem;
+}
+</style>
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
+import VueSocketio from 'vue-socket.io'
+
+import 'purecss/build/pure-min.css'
 
 import App from './App'
 import router from './router'
 import store from './store'
-import VueSocketio from 'vue-socket.io'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
